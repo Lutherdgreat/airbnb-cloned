1fbbcdd563b560729ea1688a03612b299b975d51
Lutherdgreat <adedoyin-t@webmail.uwinnpeg.ca>
2023-05-02 01:58:19 -0500

    Readme additonal updates

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Airbnb Clone App
 
-I'm currently building an Airbnb clone app using TypeScript, Tailwind CSS, and MongoDB. One of the major features I'm working on is the user authentication system for secure login and registration.
+I'm currently building an Airbnb clone app using TypeScript, Tailwind CSS, and MongoDB. Features included in the app are
+
+- User Account creation with password authentication
+- File upload with Cloudinary
+- User Registration with Google and Github
+- MongoDb to handle Registration and Possible listings
+- Location Services
 
 I made use of a YouTube tutorial by [Code With Antonio](https://youtu.be/c_-b_isI4vg) as a guide and added my personal touches to it.
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,13 @@
-import Container from "@/app/components/Container";
-import ListingCard from "@/app/components/listings/ListingCard";
-import EmptyState from "@/app/components/EmptyState";
-
-import getListings, { IListingsParams } from "@/app/actions/getListings";
-import getCurrentUser from "@/app/actions/getCurrentUser";
 import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import getListings from "./actions/getListings";
+import ListingCard from "./components/listings/ListingCard";
+import getCurrentuser from "./actions/getCurrentUser";
 
-interface HomeProps {
-	searchParams: IListingsParams;
-}
-
-const Home = async ({ searchParams }: HomeProps) => {
-	const listings = await getListings(searchParams);
-	const currentUser = await getCurrentUser();
+export default async function Home() {
+	const listings = await getListings();
+	const currentUser = await getCurrentuser();
 
 	if (listings.length === 0) {
 		return (
@@ -21,34 +16,31 @@ const Home = async ({ searchParams }: HomeProps) => {
 			</ClientOnly>
 		);
 	}
-
 	return (
 		<ClientOnly>
 			<Container>
 				<div
-					className="
-            pt-24
-            grid 
-            grid-cols-1 
-            sm:grid-cols-2 
-            md:grid-cols-3 
-            lg:grid-cols-4
-            xl:grid-cols-5
-            2xl:grid-cols-6
-            gap-8
-          "
+					className="pt-24
+								grid
+								grid-cols-1
+								sm:grid-cols-2
+								md:grid-cols-3
+								lg:grid-cols-4
+								xl:grid-cols-5
+								2xl:grid-cols-6
+								gap-8"
 				>
-					{listings.map((listing: any) => (
-						<ListingCard
-							currentUser={currentUser}
-							key={listing.id}
-							data={listing}
-						/>
-					))}
+					{listings.map((listing: any) => {
+						return (
+							<ListingCard
+								currentUser={currentUser}
+								key={listing.id}
+								data={listing}
+							/>
+						);
+					})}
 				</div>
 			</Container>
 		</ClientOnly>
 	);
-};
-
-export default Home;
+}
