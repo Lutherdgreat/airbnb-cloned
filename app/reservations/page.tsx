b11d0766aaed8bd759b3c35651f8252f4f67a735
Lutherdgreat <adedoyin-t@webmail.uwinnpeg.ca>
2023-05-08 02:42:30 -0500

    Added Reservation funcionality for properties

--- app/reservations/page.tsx
+++ app/reservations/page.tsx
@@ -3,5 +3,42 @@ import ClientOnly from "../components/ClientOnly";
 
 import getCurrentUser from "../actions/getCurrentUser";
 import getReservations from "../actions/getReservations";
+import ReservationsClient from "./ReservationsClient";
 
-const Reservations = async () => {};
+const ReservationsPage = async () => {
+	const currentUser = await getCurrentUser();
+
+	if (!currentUser) {
+		return (
+			<ClientOnly>
+				<EmptyState title="unauthorized" subtitle="Please login" />
+			</ClientOnly>
+		);
+	}
+
+	const reservations = await getReservations({
+		authorId: currentUser.id,
+	});
+
+	if (reservations.length === 0) {
+		return (
+			<ClientOnly>
+				<EmptyState
+					title="No reservations found"
+					subtitle="Looks like there is no current reservation on your property"
+				/>
+			</ClientOnly>
+		);
+	}
+
+	return (
+		<ClientOnly>
+			<ReservationsClient
+				reservations={reservations}
+				currentUser={currentUser}
+			/>
+		</ClientOnly>
+	);
+};
+
+export default ReservationsPage;
